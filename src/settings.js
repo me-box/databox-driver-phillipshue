30adbc410cfa41a8108065cf045eb25331ab08b3
ToshBrown <Toshbrown@gmail.com>
2018-07-19 10:26:07 +0100

    Fix settings

--- src/settings.js
+++ src/settings.js
@@ -2,50 +2,43 @@
 const databox = require('node-databox');
 
 const datasourceid = 'philipsHueSettings';
+const settingKey = 'settings';
 
-let settingsCache = null;
 
 module.exports = (keyValueClient) => {
 
   let kvc = keyValueClient;
 
   let getSettings = () => {
-  return new Promise((resolve,reject)=>{
+    return new Promise((resolve,reject)=>{
+      kvc.Read(datasourceid, "settings")
+      .then((settings)=>{
+        if(Object.keys(settings).length == 0) {
+          return Promise.reject('No setting found.');
+        }
+        console.log("[getSettings]",settingKey);
+        settingsCache = settings;
+        resolve(settings);
+      })
+      .catch((err)=>{
+        reject(err);
+      });
 
-    if(settingsCache !== null) {
-      resolve(settingsCache);
-    }
-
-    kvc.Read(datasourceid, "settings")
-    .then((settings)=>{
-      if(sObject.keys(settings).length == 0) {
-        return Promise.reject('No setting found.');
-      }
-      console.log("[getSettings]",settings);
-      settingsCache = settings;
-      resolve(settings);
-    })
-    .catch((err)=>{
-      reject(err);
     });
-
-  });
   };
 
   let setSettings = (settings) => {
+    //to do validate settings
+    return new Promise((resolve,reject)=>{
+      kvc.Write(datasourceid,"settingKey",settings)
+      .then(()=>{
+        resolve(settings);
+      })
+      .catch((err)=>{
+        reject(err);
+      });
 
-  //to do validate settings
-  return new Promise((resolve,reject)=>{
-    kvc.Write(datasourceid,"settings",settings)
-    .then(()=>{
-      settingsCache = settings;
-      resolve(settings);
-    })
-    .catch((err)=>{
-      reject(err);
     });
-
-  });
   };
 
   return {
